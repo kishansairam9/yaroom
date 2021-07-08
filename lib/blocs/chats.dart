9dcb1cdf2b218e128b9a358c7db6fd8c3d7e951d
kishansairam9 <kishansairam.adapa9@gmail.com>
2021-07-08 13:30:04 +0530

    Finished bloc for chat

--- lib/blocs/chats.dart
+++ lib/blocs/chats.dart
@@ -1,4 +1,57 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../utils/types.dart';
 
-// TODO: BLOC Implementation for modifying and getting chat messages
-// BLOC linked to one DM
+class UserChatCubit extends Cubit<List<ChatMessage>> {
+  late int otherUser;
+  late AppDb db;
+
+  UserChatCubit(
+      {required this.otherUser,
+      required this.db,
+      required List<ChatMessage> initialState})
+      : super(initialState);
+
+  void addTextMessage(
+      {required int msgId, required int fromUser, required String content}) {
+    this.db.insertTextMessage(
+        msgId: msgId,
+        fromUser: fromUser,
+        toUser: otherUser,
+        time: DateTime.now(),
+        content: content);
+    emit(state +
+        [
+          ChatMessage(
+              msgId: msgId,
+              fromUser: fromUser,
+              toUser: otherUser,
+              time: DateTime.now(),
+              content: content)
+        ]);
+  }
+
+  void addMediaMessage(
+      {required int msgId,
+      required int fromUser,
+      required String media,
+      String? content}) {
+    this.db.insertMediaMessage(
+        msgId: msgId,
+        fromUser: fromUser,
+        toUser: otherUser,
+        time: DateTime.now(),
+        media: media,
+        content: content);
+    emit(state +
+        [
+          ChatMessage(
+              msgId: msgId,
+              fromUser: fromUser,
+              toUser: otherUser,
+              time: DateTime.now(),
+              media: media,
+              content: content)
+        ]);
+  }
+}
--- lib/screens/home/chatPage.dart
+++ lib/screens/home/chatPage.dart
@@ -7,6 +7,7 @@ import 'package:yaroom/fakegen.dart';
 import '../components/contactView.dart';
 import 'package:provider/provider.dart';
 import '../../utils/types.dart';
+import '../../blocs/chats.dart';
 
 class ChatPage extends StatefulWidget {
   final userId, name, image;
@@ -16,8 +17,6 @@ class ChatPage extends StatefulWidget {
 
 class ChatPageState extends State<ChatPage> {
   late bool isShowSticker;
-  late Stream<List<ChatMessage>>
-      dataStream; // TODO: REMOVE USAGE STREAM AND USE BLOC
 
   final inputController = TextEditingController();
 
@@ -32,9 +31,6 @@ class ChatPageState extends State<ChatPage> {
   void initState() {
     super.initState();
     isShowSticker = false;
-    dataStream = RepositoryProvider.of<AppDb>(context)
-        .getUserChat(otherUser: widget.userId)
-        .watch(); // TODO: Remove this and use BLOC
   }
 
   // handling backPress when emoji keyboard is implemented
@@ -83,7 +79,7 @@ class ChatPageState extends State<ChatPage> {
         });
   }
 
-  Widget showMessages(List<ChatMessage> msgs) {
+  Widget _buildMessagesView(List<ChatMessage> msgs) {
     return Expanded(
       child: Column(
         children: [
@@ -93,149 +89,150 @@ class ChatPageState extends State<ChatPage> {
                   padding: EdgeInsets.only(top: 15.0),
                   itemCount: msgs.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return _buildMessage(msgs[index]);
+                    return _buildMessage(msgs[msgs.length - 1 - index]);
                   }))
         ],
       ),
     );
   }
 
+  void _sendMessage(BuildContext context, String data) {
+    // TODO: Get msg id by sending data to server
+    BlocProvider.of<UserChatCubit>(context, listen: false).addTextMessage(
+        msgId: getMsgId(),
+        fromUser: Provider.of<UserId>(context, listen: false),
+        content: data);
+  }
+
   Widget build(BuildContext context) {
-    return WillPopScope(
-      child: Stack(children: <Widget>[
-        Scaffold(
-            appBar: AppBar(
-              titleSpacing: 0,
-              title: ListTile(
-                onTap: () => _showContact(context),
-                contentPadding: EdgeInsets.only(left: 0.0, right: 0.0),
-                tileColor: Colors.transparent,
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey[350],
-                  foregroundImage: NetworkImage('${widget.image}'),
-                  backgroundImage: AssetImage('assets/no-profile.png'),
-                ),
-                title: Text(
-                  widget.name,
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              actions: <Widget>[
-                IconButton(
-                  onPressed: () => {},
-                  icon: Icon(Icons.phone),
-                  tooltip: 'Call',
-                ),
-                IconButton(
-                  onPressed: () => {},
-                  icon: Icon(Icons.more_vert),
-                  tooltip: 'More',
-                )
-              ],
-            ),
-            body: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  StreamBuilder(
-                      stream: dataStream,
-                      builder: (BuildContext context,
-                          AsyncSnapshot<List<ChatMessage>> snapshot) {
-                        if (snapshot.hasData) {
-                          return showMessages(snapshot.data!);
-                        } else if (snapshot.hasError) {
-                          print(snapshot.data);
-                          return SnackBar(
-                              content: Text(
-                                  'Error has occured while reading from DB'));
-                        }
-                        return Container();
-                      }),
-                  Row(
-                    children: [
-                      Expanded(
-                          child: RawKeyboardListener(
-                              focusNode: FocusNode(),
-                              onKey: (RawKeyEvent event) async {
-                                if (kIsWeb ||
-                                    Platform.isMacOS ||
-                                    Platform.isLinux ||
-                                    Platform.isWindows) {
-                                  // Submit on Enter and new line on Shift + Enter only on desktop devices or Web
-                                  if (event.isKeyPressed(
-                                          LogicalKeyboardKey.enter) &&
-                                      !event.isShiftPressed) {
-                                    // TODO: EXTRACT SAME CALL INTO ONE THING 3 times duplicated
-                                    // TODO: Get msg id by sending data to server
-                                    String data = inputController.text;
-                                    inputController.clear();
-                                    await RepositoryProvider.of<AppDb>(context)
-                                        .insertTextMessage(
-                                            msgId: getMsgId(),
-                                            fromUser: Provider.of<UserId>(
-                                                context,
-                                                listen: false),
-                                            toUser: widget.userId,
-                                            time: DateTime.now(),
-                                            content: data.trim());
-                                  }
-                                }
-                              },
-                              child: TextField(
-                                maxLines: null,
-                                controller: inputController,
-                                textCapitalization:
-                                    TextCapitalization.sentences,
-                                onEditingComplete: () async {
-                                  // TODO: EXTRACT SAME CALL INTO ONE THING 3 times duplicated
-                                  // TODO: Get msg id by sending data to server
-                                  String data = inputController.text;
-                                  inputController.clear();
-                                  await RepositoryProvider.of<AppDb>(context)
-                                      .insertTextMessage(
-                                          msgId: getMsgId(),
-                                          fromUser: Provider.of<UserId>(context,
-                                              listen: false),
-                                          toUser: widget.userId,
-                                          time: DateTime.now(),
-                                          content: data.trim());
-                                },
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    hintText: 'Type a message'),
-                              ))),
-                      IconButton(
-                          onPressed: () async {
-                            // TODO: EXTRACT SAME CALL INTO ONE THING 3 times duplicated
-                            // TODO: Get msg id by sending data to server
-                            String data = inputController.text;
-                            inputController.clear();
-                            await RepositoryProvider.of<AppDb>(context)
-                                .insertTextMessage(
-                                    msgId: getMsgId(),
-                                    fromUser: Provider.of<UserId>(context,
-                                        listen: false),
-                                    toUser: widget.userId,
-                                    time: DateTime.now(),
-                                    content: data.trim());
-                            var k = await RepositoryProvider.of<AppDb>(context)
-                                .searchChatMessages(query: data, limit: 5)
-                                .get();
-                            for (var x in k) {
-                              print(x.fromUser);
-                              print(x.content);
-                            }
-                            print(k);
-                          },
-                          icon: Icon(Icons.send))
-                    ],
-                  ),
-                  (isShowSticker ? buildSticker() : Container())
-                ]))
-      ]),
-      onWillPop: onBackPress,
-    );
+    return FutureBuilder(
+        future: RepositoryProvider.of<AppDb>(context)
+            .getUserChat(otherUser: widget.userId)
+            .get(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<ChatMessage>> snapshot) {
+          if (snapshot.hasData) {
+            return BlocProvider(
+                create: (context) => UserChatCubit(
+                    otherUser: widget.userId,
+                    db: RepositoryProvider.of<AppDb>(context),
+                    initialState: snapshot.data!),
+                child: Builder(builder: (context) {
+                  return WillPopScope(
+                    child: Stack(children: <Widget>[
+                      Scaffold(
+                          appBar: AppBar(
+                            titleSpacing: 0,
+                            title: ListTile(
+                              onTap: () => _showContact(context),
+                              contentPadding:
+                                  EdgeInsets.only(left: 0.0, right: 0.0),
+                              tileColor: Colors.transparent,
+                              leading: CircleAvatar(
+                                backgroundColor: Colors.grey[350],
+                                foregroundImage:
+                                    NetworkImage('${widget.image}'),
+                                backgroundImage:
+                                    AssetImage('assets/no-profile.png'),
+                              ),
+                              title: Text(
+                                widget.name,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            actions: <Widget>[
+                              IconButton(
+                                onPressed: () => {},
+                                icon: Icon(Icons.phone),
+                                tooltip: 'Call',
+                              ),
+                              IconButton(
+                                onPressed: () => {},
+                                icon: Icon(Icons.more_vert),
+                                tooltip: 'More',
+                              )
+                            ],
+                          ),
+                          body: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                BlocBuilder<UserChatCubit, List<ChatMessage>>(
+                                    builder: (BuildContext context,
+                                            List<ChatMessage> state) =>
+                                        _buildMessagesView(state)),
+                                Row(
+                                  children: [
+                                    Expanded(
+                                        child: RawKeyboardListener(
+                                            focusNode: FocusNode(),
+                                            onKey: (RawKeyEvent event) {
+                                              if (kIsWeb ||
+                                                  Platform.isMacOS ||
+                                                  Platform.isLinux ||
+                                                  Platform.isWindows) {
+                                                // Submit on Enter and new line on Shift + Enter only on desktop devices or Web
+                                                if (event.isKeyPressed(
+                                                        LogicalKeyboardKey
+                                                            .enter) &&
+                                                    !event.isShiftPressed) {
+                                                  String data =
+                                                      inputController.text;
+                                                  inputController.clear();
+                                                  _sendMessage(
+                                                      context, data.trim());
+                                                }
+                                              }
+                                            },
+                                            child: TextField(
+                                              maxLines: null,
+                                              controller: inputController,
+                                              textCapitalization:
+                                                  TextCapitalization.sentences,
+                                              onEditingComplete: () {
+                                                String data =
+                                                    inputController.text;
+                                                inputController.clear();
+                                                _sendMessage(
+                                                    context, data.trim());
+                                              },
+                                              decoration: InputDecoration(
+                                                  border: OutlineInputBorder(),
+                                                  hintText: 'Type a message'),
+                                            ))),
+                                    IconButton(
+                                        onPressed: () {
+                                          String data = inputController.text;
+                                          inputController.clear();
+                                          _sendMessage(context, data.trim());
+                                          // var k = await RepositoryProvider.of<
+                                          //         AppDb>(context)
+                                          //     .searchChatMessages(
+                                          //         query: data, limit: 5)
+                                          //     .get();
+                                          // for (var x in k) {
+                                          //   print(x.fromUser);
+                                          //   print(x.content);
+                                          // }
+                                          // print(k);
+                                        },
+                                        icon: Icon(Icons.send))
+                                  ],
+                                ),
+                                (isShowSticker ? buildSticker() : Container())
+                              ]))
+                    ]),
+                    onWillPop: onBackPress,
+                  );
+                }));
+          } else if (snapshot.hasError) {
+            print(snapshot.data);
+            return SnackBar(
+                content: Text('Error has occured while reading from local DB'));
+          }
+          return CircularProgressIndicator();
+        });
   }
 
   // create a emoji keyboard
