871f0ff6cd0ee290294017934ad40205f0289c1d
kishansairam9 <kishansairam.adapa9@gmail.com>
2021-07-18 06:39:00 +0530

    Add user id extraction from jwt

--- backend/main.go
+++ backend/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/form3tech-oss/jwt-go"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
 )
 
 func checkJWT() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		if err := jwtMiddleware.CheckJWT(c.Writer, c.Request); err != nil {
-			c.AbortWithStatus(401)
+	return func(g *gin.Context) {
+		if err := jwtMiddleware.CheckJWT(g.Writer, g.Request); err != nil {
+			g.AbortWithStatus(401)
 		}
 	}
 }
@@ -25,7 +27,15 @@ func main() {
 	})
 
 	r.GET("/secured/ping", checkJWT(), func(g *gin.Context) {
-		g.JSON(200, gin.H{"text": "Hello from private"})
+		tokContext := g.Request.Context().Value("user")
+		claims := tokContext.(*jwt.Token).Claims.(jwt.MapClaims)
+		userId, ok := claims["userId"].(string)
+		if !ok {
+			log.Info().Str("where", "post JWT verify, userID extraction").Str("error", "token missing fields").Msg("Field userID not found in recieved JWT")
+			g.AbortWithStatus(400)
+			return
+		}
+		g.JSON(200, gin.H{"text": "Hello from private " + userId})
 	})
 
 	r.GET("/", func(c *gin.Context) {
