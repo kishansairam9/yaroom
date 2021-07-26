0b4bd0043a276e0d46938885f4836898f6cfecd3
MKA-12 <kalyanadithya12@gmail.com>
2021-07-27 00:57:23 +0530

    Removed dead code in router

--- frontend/lib/utils/router.dart
+++ frontend/lib/utils/router.dart
@@ -3,22 +3,17 @@ import 'package:provider/provider.dart';
 import '../screens/home/tabs.dart';
 import 'errorPage.dart';
 import '../screens/rooms/room.dart';
-import '../auth.dart';
 import '../login.dart';
-import '../authentication.dart';
 // TODO: Currently visiting any random route on web crashes the app FIX THIS
 
 class ContentRouter {
   Route onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/signin':
-        // return MaterialPageRoute(builder: (_) => PKCEPage());
         return MaterialPageRoute(
             builder: (_) => Consumer<LandingViewModel>(
                 builder: (_, LandingViewModel viewModel, __) =>
                     LandingPage(viewModel)));
-      case '/login':
-        return MaterialPageRoute(builder: (_) => Login());
       case '/tabs':
         return MaterialPageRoute(builder: (_) => TabView());
       case '/rooms':
