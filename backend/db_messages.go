7a8493c099c6d2212a8bd949b80c596ff8fa1e8f
Kishan Sairam Adapa <kishansairam.adapa9@gmail.com>
2021-07-29 07:35:23 +0530

    Minor refactoring, finish media acess for group and rooms

---
+++ backend/db_messages.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"sort"
+	"time"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/rs/xid"
+	"github.com/rs/zerolog/log"
+	"github.com/scylladb/gocqlx/v2"
+	"github.com/scylladb/gocqlx/v2/table"
+)
+
+var ChatMessageMetadata = table.Metadata{
+	Name:    "yaroom.chat_messages",
+	Columns: []string{"exchange_id", "msgid", "fromuser", "touser", "msgtime", "content", "mediaid", "replyto", "es_query", "es_options"},
+	PartKey: []string{"exchange_id"},
+	SortKey: []string{"msgid"},
+}
+
+var GroupsMessageMetadata = table.Metadata{
+	Name:    "yaroom.groups_messages",
+	Columns: []string{"exchange_id", "msgid", "fromuser", "groupid", "msgtime", "content", "mediaid", "replyto", "es_query", "es_options"},
+	PartKey: []string{"exchange_id"},
+	SortKey: []string{"msgid"},
+}
+
+var RoomsMessageMetadata = table.Metadata{
+	Name:    "yaroom.rooms_messages",
+	Columns: []string{"exchange_id", "msgid", "fromuser", "roomid", "channelid", "msgtime", "content", "mediaid", "replyto", "es_query", "es_options"},
+	PartKey: []string{"exchange_id"},
+	SortKey: []string{"msgid"},
+}
+
+var ChatMessageTable *table.Table
+
+var RoomsMessageTable *table.Table
+
+var GroupsMessageTable *table.Table
+
+var InsertChatMessage *gocqlx.Queryx
+
+var InsertGroupsMessage *gocqlx.Queryx
+
+var InsertRoomsMessage *gocqlx.Queryx
+
+type ChatMessage struct {
+	Exchange_id string    `json:"exchange_id,omitempty"`
+	Msgid       string    `json:"msgId,omitempty"`
+	Fromuser    string    `json:"fromUser"`
+	Touser      string    `json:"toUser"`
+	Msgtime     time.Time `json:"time"`
+	Content     *string   `json:"content,omitempty"`
+	Mediaid     *string   `json:"media,omitempty"`
+	Replyto     *string   `json:"replyTo,omitempty"`
+	Es_query    string    `json:"es_query,omitempty"`
+	Es_options  string    `json:"es_options,omitempty"`
+}
+
+type GroupsMessage struct {
+	Exchange_id string    `json:"exchange_id,omitempty"`
+	Msgid       string    `json:"msgId,omitempty"`
+	Fromuser    string    `json:"fromUser"`
+	Groupid     string    `json:"groupId"`
+	Msgtime     time.Time `json:"time"`
+	Content     *string   `json:"content,omitempty"`
+	Mediaid     *string   `json:"media,omitempty"`
+	Replyto     *string   `json:"replyTo,omitempty"`
+	Es_query    string    `json:"es_query,omitempty"`
+	Es_options  string    `json:"es_options,omitempty"`
+}
+
+type RoomsMessage struct {
+	Exchange_id string    `json:"exchange_id,omitempty"`
+	Msgid       string    `json:"msgId,omitempty"`
+	Fromuser    string    `json:"fromUser"`
+	Roomid      string    `json:"roomid"`
+	Channelid   string    `json:"channelid"`
+	Msgtime     time.Time `json:"time"`
+	Content     *string   `json:"content,omitempty"`
+	Mediaid     *string   `json:"media,omitempty"`
+	Replyto     *string   `json:"replyTo,omitempty"`
+	Es_query    string    `json:"es_query,omitempty"`
+	Es_options  string    `json:"es_options,omitempty"`
+}
+
+func getExchangeId(msg *WSMessage) (string, error) {
+	switch msg.Type {
+	case "ChatMessage":
+		uids := []string{msg.FromUser, msg.ToUser}
+		sort.Strings(uids)
+		return uids[0] + ":" + uids[1], nil
+	case "GroupsMessage":
+		uids := []string{msg.GroupId}
+		sort.Strings(uids)
+		return uids[0], nil
+	case "RoomsMessage":
+		uids := []string{msg.RoomId}
+		sort.Strings(uids)
+		return uids[0], nil
+	default:
+		return "", errors.New("unknown message type")
+	}
+}
+
+func addMessage(msg *WSMessage) error {
+	// Get exchange id
+	exchange_id, err := getExchangeId(msg)
+	if err != nil {
+		return err
+	}
+
+	// Handle media content
+	if msg.MediaData != nil {
+		if msg.MediaData.Name == "" || len(msg.MediaData.Bytes) == 0 {
+			return errors.New("invalid media file, check non empty name, bytes > 0")
+		}
+		// TODO: COMPRESS AND STORE TO SAVE STORAGE SPACE (LATER)
+		mediaBytes, _ := json.Marshal(msg.MediaData)
+		mediaId := xid.New().String()
+
+		if _, err := minioClient.PutObject(context.Background(), miniobucket, mediaId, bytes.NewReader(mediaBytes), -1, minio.PutObjectOptions{ContentType: "application/json", UserMetadata: map[string]string{"x-amz-meta-key": exchange_id}}); err != nil {
+			log.Error().Str("where", "ws read routine").Str("type", "uploading to minio failed at put object").Msg(err.Error())
+			return errors.New("internal server error")
+		}
+		msg.Media = mediaId
+		msg.MediaData = nil
+	}
+
+	// Use current time of server // We don't care about user sending time, we use receiving time
+	msg.Time = time.Now()
+	jsonBytes, _ := json.Marshal(msg)
+
+	msgId := xid.New().String()
+
+	// Handle different message types
+	switch msg.Type {
+	case "ChatMessage":
+		var data ChatMessage
+		if err := json.Unmarshal(jsonBytes, &data); err != nil {
+			return err
+		}
+		data.Exchange_id = exchange_id
+		data.Msgid = msgId
+		data.Msgtime = time.Now()
+		if q := InsertChatMessage.BindStruct(data); q.Err() != nil {
+			log.Error().Str("where", "insert chat message").Str("type", "failed to bind struct").Msg(q.Err().Error())
+			return errors.New("internal server error")
+		}
+		if err := InsertChatMessage.Exec(); err != nil {
+			log.Error().Str("where", "insert chat message").Str("type", "failed to execute query").Msg(err.Error())
+			return errors.New("internal server error")
+		}
+	case "GroupsMessage":
+		var data GroupsMessage
+		if err := json.Unmarshal(jsonBytes, &data); err != nil {
+			return err
+		}
+		data.Exchange_id = exchange_id
+		data.Msgid = msgId
+		data.Msgtime = time.Now()
+		if q := InsertGroupsMessage.BindStruct(data); q.Err() != nil {
+			log.Error().Str("where", "insert chat message").Str("type", "failed to bind struct").Msg(q.Err().Error())
+			return errors.New("internal server error")
+		}
+		if err := InsertGroupsMessage.Exec(); err != nil {
+			log.Error().Str("where", "insert groups message").Str("type", "failed to execute query").Msg(err.Error())
+			return errors.New("internal server error")
+		}
+	case "RoomsMessage":
+		var data RoomsMessage
+		if err := json.Unmarshal(jsonBytes, &data); err != nil {
+			return err
+		}
+		data.Exchange_id = exchange_id
+		data.Msgid = msgId
+		data.Msgtime = time.Now()
+		if q := InsertRoomsMessage.BindStruct(data); q.Err() != nil {
+			log.Error().Str("where", "insert rooms message").Str("type", "failed to bind struct").Msg(q.Err().Error())
+			return errors.New("internal server error")
+		}
+		if err := InsertRoomsMessage.Exec(); err != nil {
+			log.Error().Str("where", "insert rooms message").Str("type", "failed to execute query").Msg(err.Error())
+			return errors.New("internal server error")
+		}
+	default:
+		return errors.New("unknown message type")
+	}
+
+	msg.MsgId = msgId
+	return nil
+}
+
+func dbInsertRoutine(inChannel <-chan interface{}, outChannel chan<- interface{}, quit <-chan bool) {
+	var err error
+	for {
+		select {
+		case data := <-inChannel:
+			msg, _ := data.(WSMessage)
+			err = addMessage(&msg)
+			if err != nil {
+				var emsg string
+				if err.Error() == "unknown message type" {
+					emsg = "Unknown message type"
+				} else {
+					emsg = "Server error, contact support"
+					log.Error().Str("where", "add message").Str("type", "failed to add messsage to db").Msg(err.Error())
+				}
+				outChannel <- WSError{Error: emsg}
+			} else {
+				outChannel <- msg
+			}
+		case <-quit:
+			return
+		}
+	}
+}
---
+++ backend/fcm_notifications.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/appleboy/go-fcm"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
+
+// TODO: Remove receiving name and image, backend should have it already
+func fcmTokenHandler(g *gin.Context) {
+	var req fcmTokenUpdate
+	if err := g.BindJSON(&req); err != nil {
+		log.Info().Str("where", "bind json").Str("type", "failed to parse body to json").Msg(err.Error())
+		return
+	}
+
+	rawUserId, exists := g.Get("userId")
+	if !exists {
+		print("what?")
+		g.AbortWithStatusJSON(400, gin.H{"error": "not authenticated"})
+		return
+	}
+	userId := rawUserId.(string)
+
+	if err := addFCMToken(&UserFCMTokenUpdate{Userid: userId, Tokens: []string{req.Token}, Name: req.Name, Image: req.Image}); err != nil {
+		g.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+		return
+	}
+}
+
+func trimLength(content string, length int) string {
+	if len(content) > length {
+		content = content[:length] + "..."
+	}
+	return content
+}
+
+func sendMessageNotification(userId string, msg WSMessage) error {
+	fmt.Printf("sending fcm message to %v\n", userId)
+	encodedString, _ := json.Marshal(msg)
+	var data map[string]interface{}
+	json.Unmarshal(encodedString, &data)
+
+	var fromUserData *UserMetadata
+	var toUserData *UserMetadata
+	// TODO: Uncomment after mocking
+	// fromUserData, err := getUserMetadata(msg.FromUser)
+	// if err != nil {
+	// 	return err
+	// }
+	dummy := "dmmy"
+	fromUserData = &UserMetadata{Name: dummy}
+	toUserData, err := getUserMetadata(userId)
+	if err != nil {
+		return err
+	}
+	// TODO Remove debug statements
+	fmt.Println(toUserData.Name)
+	fmt.Println(toUserData.Userid)
+	fmt.Println(fromUserData.Image)
+	fmt.Print("To user tokens -----  ")
+	fmt.Println(toUserData.Tokens)
+	if len(toUserData.Tokens) == 0 {
+		return errors.New("user doesn't exits or token for notification not present")
+	}
+
+	switch msg.Type {
+	case "ChatMessage":
+		if fromUserData.Image == nil || *fromUserData.Image == "" {
+			testImg := "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+			fromUserData.Image = &testImg
+		}
+		for _, token := range toUserData.Tokens {
+			print("sending to ", token)
+			notif := &fcm.Message{
+				To:   token,
+				Data: data,
+				Notification: &fcm.Notification{
+					Title: fromUserData.Name,
+					Body:  trimLength(msg.Content, 150),
+					Image: *fromUserData.Image,
+				},
+			}
+			res, err := fcmClient.SendWithRetry(notif, 3)
+			if err == fcm.ErrInvalidRegistration {
+				print("remove token ", token)
+				removeFCMToken(&UserFCMTokenUpdate{Userid: userId, Tokens: []string{token}})
+			} else if err != nil {
+				log.Error().Str("where", "update user metadata").Str("type", "failed to bind struct").Str("fcm response", fmt.Sprint(res)).Msg(err.Error())
+			}
+		}
+
+	default:
+		return errors.New("unknown message type")
+	}
+	return nil
+}
